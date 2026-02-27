5071b0d73bf810efc420aa16f11de48a2776582a
Tara <tara.ana.fosnaric@gmail.com>
2026-02-27 15:30:35 +0100

    izpis prastevil z do pa while, ne tega delat

---
+++ Predavanje/27_02/do_while.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+/* ne tega delat */
+
+bool isprime(int n) {
+    int d = 2;
+    do {
+        if (d <= n - 1 && n % d == 0)
+            return false;
+        d = d + 1;
+    } while (d <= n - 1);
+   
+    return true;
+}
+
+int main() {
+    for (int n = 1; n <= 100; n++ ) {
+        if (isprime(n)) printf("%d ", n);
+    }
+    printf("\n"); 
+    return 0;
+}
