97a6447c08c80c936316eb7a566033e18be36bf3
Tara <tara.ana.fosnaric@gmail.com>
2026-02-27 15:22:16 +0100

    program ki izpise prestevila med 1-100

---
+++ Predavanje/27_02/izpis_prastevil.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isprime(int n) {
+    for (int d = 2; d <= n - 1; d++) {
+        if (n % d == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    for (int n = 1; n <= 100; n++ ) {
+        if (isprime(n)) printf("%d ", n);
+    }
+    printf("\n"); 
+    return 0;
+}
