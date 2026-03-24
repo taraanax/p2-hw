b06f6b15b5de6f9b32467a38424c0c375f9d76e4
Tara <tara.ana.fosnaric@gmail.com>
2026-03-25 00:13:35 +0100

    first time delam s kazalci, kinda nervous

---
+++ Predavanje/13_03/kazalci1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int n = 17;
+int m;
+
+int f() {
+    
+    int a = 3;
+    return a;
+}
+
+int main () {
+
+    int *p;
+    p = &n;
+    printf("%p\n", p);
+
+    m = *p;
+
+    return 0;
+}
