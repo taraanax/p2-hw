db1d288867eac64dd19db33230a9046fa57db3c0
Tara Ana <tara.ana.fosnaric@gmail.com>
2026-05-14 09:33:27 +0200

    done

--- Vaje/vaje10/naloga.c
+++ Vaje/vaje10/naloga.c
@@ -26,8 +26,37 @@ int poisciPO(Student* student, char* predmet) {
 }
 
 int dodaj(Student** studentje, int stStudentov, int vpisna, char* predmet, int ocena) {
+    int s = poisciStudenta(studentje, stStudentov, vpisna);
     
-    return 42;
+    if (s == -1){
+        Student* nov = malloc(sizeof(Student));
+        nov->vpisna = vpisna;
+        nov->stPO = 1;
+        PO* po = calloc(10, sizeof(PO));
+        nov->po = po;
+        strcpy(po[0].predmet, predmet);
+        po[0].ocena = ocena;
+
+        studentje[stStudentov] = nov;
+
+        return stStudentov + 1;
+    }
+    
+    int p = poisciPO(studentje[s], predmet);
+    PO* po = studentje[s]->po;
+
+    if (p > -1) {
+        po[p].ocena = ocena;
+    } else {
+        Student* st = studentje[s];
+        int i = st->stPO;
+
+        strcpy(st->po[i].predmet, predmet);
+        st->po[i].ocena = ocena;
+
+        st->stPO++;
+    }
+    return stStudentov;
 }
 
 #ifndef test
