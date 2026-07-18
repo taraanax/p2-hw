6e74ec5903f1871a80594b53a7580ea65efb35fa
Tara Ana <tara.ana.fosnaric@gmail.com>
2026-07-18 17:37:10 +0200

    2 / 10

--- DN/DN04/naloga2/naloga2.c
+++ DN/DN04/naloga2/naloga2.c
@@ -17,29 +17,51 @@ int main() {
         scanf("%d", &arr[i]);
     }
     
+    /*printf("arr: ");
+    for (int i = 0; i < n; i++) {
+        printf("[%d] ", arr[i]);
+    }
+    printf(" \n");
+    */
+
     int count = 0;
     int levo = 0;
     int desno = n - 1;
 
     while (levo < desno) {
         int sum = arr[levo] + arr[desno];
-        if (sum == k) count++;
-        else if (sum >= k) {
+        
+        if (sum == k) {
+            //printf("(%d, %d) ", arr[levo], arr[desno]);
+
+            if(arr[levo] != arr[desno]){
+                int levoEnaki = 0;
+                while (levo + 1 < desno && arr[levo+1] == arr[levo]) {
+                    levoEnaki++;
+                    levo++;
+                }
+
+                int desnoEnaki = 0;
+                while (levo < desno - 1 && arr[desno-1] == arr[desno]) {
+                    desnoEnaki++;
+                    desno--;
+                }
+
+                count = count + (desnoEnaki * levoEnaki);
+                levo++;
+                desno--;
+
+            } else {
+                count = (n * (n - 1)) / 2;
+                break;
+            }
+            
+        } else if (sum >= k) {
             desno--;
         } else levo++;
     }
-    
+    //printf(" \n");
     printf("%d\n", count);
 
-/*debugging
-    printf(" \n");
-
-    
-
-    printf("arr: ");
-    for (int i = 0; i < n; i++) {
-        printf("[%d] ", arr[i]);
-    }
-*/
     return -1;
 }
