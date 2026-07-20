bacef8fdfddbe0bdf8d29fe34819b46e666651fd
Tara Ana <tara.ana.fosnaric@gmail.com>
2026-07-20 04:25:47 +0200

    10 / 10

--- DN/DN05/naloga2/naloga2.c
+++ DN/DN05/naloga2/naloga2.c
@@ -20,8 +20,25 @@ void zamenjaj(int** a, int** b) {
 }
 
 void uredi(int** a, int** b, int** c) {
-    // dopolnite ...
+    int* temp;
+   
+    if (b[0][0] > c[0][0]) {
+        temp = c[0];
+        c[0] = b[0];
+        b[0] = temp;
+    }
+    if (a[0][0] > c[0][0]) {
+        temp = c[0];
+        c[0] = a[0];
+        a[0] = temp;
+    }
+    if (a[0][0] > b[0][0]) {
+        temp = b[0];
+        b[0] = a[0];
+        a[0] = temp;
+    }
 }
+
 
 #ifndef test
 
