2e62e73e9aa2164812711ad5b585c8091f53c55a
Tara Ana <tara.ana.fosnaric@gmail.com>
2026-07-20 04:29:37 +0200

    dn06 izhodisca

---
+++ DN/DN06/naloga1/naloga1.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "naloga1.h"
+
+char* zdruzi(char** nizi, char* locilo) {
+    // popravite / dopolnite ...
+    return NULL;
+}
+
+#ifndef test
+
+int main() {
+    // koda za ro"cno testiranje
+    return 0;
+}
+
+#endif
