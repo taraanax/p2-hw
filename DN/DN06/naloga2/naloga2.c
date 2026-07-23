ba97dfa30d267ea93cb3cf6ec8fe7bea67b9d998
Tara Ana <tara.ana.fosnaric@gmail.com>
2026-07-23 16:19:12 +0200

    5 / 10

--- DN/DN06/naloga2/naloga2.c
+++ DN/DN06/naloga2/naloga2.c
@@ -7,8 +7,20 @@
 #include "naloga2.h"
 
 int** ap2pp(int(*kazalec)[N], int izvornoStVrstic, int ciljnoStVrstic) {
-    // popravite / dopolnite
-    return NULL;
+    int nov_N = (N * izvornoStVrstic) / ciljnoStVrstic + 1;
+
+    int** resitev = malloc(ciljnoStVrstic * sizeof(int*));
+    
+    int k = 0;
+    for (int i = 0; i < ciljnoStVrstic; i++) {
+        resitev[i] = calloc(nov_N, sizeof(int));
+        for (int j = 0; j < nov_N - 1; j++) {
+            resitev[i][j] = kazalec[k/N][k%N];
+            k++;
+       } 
+    }
+
+    return resitev;
 }
 
 int (*pp2ap(int** kazalec, int izvornoStVrstic, int* ciljnoStVrstic))[N] {
