c3bef71ad4f52755c648c4e306473c91d6d13f7e
Tara Ana <tara.ana.fosnaric@gmail.com>
2026-07-30 19:58:58 +0200

    input

--- DN/DN07/naloga1/naloga1.c
+++ DN/DN07/naloga1/naloga1.c
@@ -4,23 +4,34 @@
 #include <stdbool.h>
 
 typedef struct _node {
-        int value;
         struct _node* levo;
         struct _node* desno;
     } node;
 
 int main () {
 
-    int n = scanf("%d", &n);
-
-    node* koren = malloc(sizeof(node));
-    koren->value = 0;
-    koren->levo = scanf("%d", &koren->levo);
-    koren->desno = scanf("%d", &koren->desno);
+    int n; 
+    scanf("%d", &n);
     
-    for (int i = 2; i < n; i++)  {
-        
+    node* vozlisca[1000];
+    for (int i = 0; i < n; i++) {
+        vozlisca[i] = malloc(sizeof(node));
+        vozlisca[i]->levo = NULL;
+        vozlisca[i]->desno = NULL;
     }
+
+    for (int i = 0; i < n; i++) {
+        int l, d;
+        scanf("%d %d", &l, &d);
+
+        if (l != -1) vozlisca[i]->levo = vozlisca[l];
+        
+        if (d != -1) vozlisca[i]->desno = vozlisca[d];
+    }
+
+    node* koren = vozlisca[0];
+
+    
 
 
 
