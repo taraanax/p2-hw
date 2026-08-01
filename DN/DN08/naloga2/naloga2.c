01ddaa198036a1e338bb4a243da4831f34e12418
Tara Ana <tara.ana.fosnaric@gmail.com>
2026-08-01 18:53:58 +0200

    prinf fix 8 / 10

--- DN/DN08/naloga2/naloga2.c
+++ DN/DN08/naloga2/naloga2.c
@@ -3,6 +3,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+bool zmaga(int n, int k) {
+    if (n == 0) return false;
+
+    for (int i = 1; i < k + 1 && i < n + 1; i++) {
+        if (!zmaga(n - i, k))
+            return true;
+    }
+
+    return false;
+}
+
 void append(int* poteze, int poteza) {
     int i = 0;
     while(poteze[i] != 0) i++;
@@ -17,46 +28,38 @@ void back(int* poteze, int poteza) {
     return;
 }
 
-bool zmaga(int n, int k) {
-    if (n == 0) return false;
-
-    for (int i = 1; i <= k && i <= n; i++) {
-        if (!zmaga(n - i, k))
-            return true;
-    }
-
-    return false;
-}
-
 void resitev(int n, int k, bool beli, int* poteze) {
     if (n == 0 && beli == true) {
         printf("CRNI");
         return;
     }
     if (n == 0 && beli == false) {
-        printf("%d ->", poteze[0]);
+        printf("%d", poteze[0]);
         int i = 1;
         while (poteze[i+1] != 0) {
-            printf(" [%d] -> %d", poteze[i], poteze[i+1]);
+            printf(" -> [%d] -> %d", poteze[i], poteze[i+1]);
             i = i + 2;
         }
+        printf("\n");
         return;
     }
     if (beli == true) {
-        for (int i = 1; i < k + 1; i++) {
-            append(poteze, i);
-            resitev(n - i, k, !beli, poteze);
-            back(poteze, i);
+        for (int i = 1; i < k + 1 && i < n + 1; i++) {
+            if (!zmaga(n - i, k)) {
+                append(poteze, i);
+                resitev(n - i, k, !beli, poteze);
+                back(poteze, i);
+            }
         }
-        return;
     }
     if (beli == false) {
-        for (int i = 1; i < k + 1; i++) {
-            append(poteze, i);
-            resitev(n - i, k, !beli, poteze);
-            back(poteze, i);
+        for (int i = 1; i < k + 1 && i < n + 1; i++) {
+            if (zmaga(n - i, k)) {
+                append(poteze, i);
+                resitev(n - i, k, !beli, poteze);
+                back(poteze, i);
+            }
         }
-        return;
     }
 }
 
