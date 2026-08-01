64d5c5ddeb52e35f9ea268d5f502f141d9788479
Tara Ana <tara.ana.fosnaric@gmail.com>
2026-08-01 17:33:33 +0200

    2 / 10

--- DN/DN08/naloga2/naloga2.c
+++ DN/DN08/naloga2/naloga2.c
@@ -3,11 +3,42 @@
 #include <stdlib.h>
 #include <string.h>
 
+void append(int* poteze, int poteza) {
+    int i = 0;
+    while(poteze[i] != 0) i++;
+    poteze[i] = poteza;
+    return;
+}
+
+void resitev(int n, int k, bool beli, int* poteze) {
+    if (n == 0 && beli == true) {
+        printf("CRNI");
+        return;
+    }
+    if (n == 0 && beli == false) {
+        printf("%d ->", poteze[0]);
+        int i = 1;
+        while (poteze[i+1] != 0) {
+            printf(" [%d] -> %d", poteze[i], poteze[i+1]);
+            i = i + 2;
+        }
+        return;
+    } else {
+        for (int i = 1; i < k + 1; i++) {
+            append(poteze, i);
+            resitev(n - i, k, !beli, poteze);
+        }
+        return;
+    }
+}
+
 int main () {
     int n, k;
     scanf("%d %d", &n, &k);
 
-    if (n - k == 1 || ((n % 2 == 0) && k == 1)) printf("CRNI");
+    int* poteze = calloc(n, sizeof(int));
+
+    resitev(n,k,true,poteze);
 
     return 0;
 }
