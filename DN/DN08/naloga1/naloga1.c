9f59ac5703d76a92308f2250526d634f6291c307
Tara Ana <tara.ana.fosnaric@gmail.com>
2026-08-01 02:40:34 +0200

    isSorted

--- DN/DN08/naloga1/naloga1.c
+++ DN/DN08/naloga1/naloga1.c
@@ -3,6 +3,27 @@
 #include <stdlib.h>
 #include <string.h>
 
+bool isSorted(int* zap, int n) {
+    for (int i = 0; i < n-1; i++) {
+        if (zap[i] > zap[i+1]) 
+            return false;
+    }
+    return true;
+}
+
+int* swap(int* zap, int n, int r, int index){
+    
+}
+
+int resitev(int n, int k, int r, int* zap, int index) {
+    if (isSorted(zap, n) && k >= 0) return 1;
+    if (isSorted(zap, n) && k < 0) return 0;
+    else {
+
+    }
+    return 0;
+}
+
 int main () {
 
     int n, k, r;
@@ -16,6 +37,9 @@ int main () {
     for (int i = 0; i < n; i++) {
         printf("%d ", zap[i]);
     }
+
+    resitev(n, k, r, zap, 0);
+
 
     return 0;
 }
