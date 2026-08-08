1431644e5813a2d78d1b3227906dcc570abb5778
Tara Ana <tara.ana.fosnaric@gmail.com>
2026-08-08 19:15:18 +0200

    1 / 10

--- DN/dn10/naloga1/naloga1.c
+++ DN/dn10/naloga1/naloga1.c
@@ -7,8 +7,30 @@
 #include "naloga1.h"
 
 VO** opravili(Student** studentje, int stStudentov, char* predmet, int* stVO) {
-    // popravite / dopolnite ...
-    return NULL;
+    
+    VO** opravili = malloc(stStudentov * sizeof(VO*));
+    stVO[0] = 0;
+    int k = 0;
+
+    for (int i = 0; i < stStudentov; i++) {
+        Student* student = studentje[i];
+        PO* po = student->po;
+        for (int j = 0; j < student->stPO; j++) {
+            if(strcmp((po[j]).predmet, predmet) == 0 && (po[j]).ocena > 5) {
+                opravili[k] = malloc(sizeof(VO));
+                (opravili[k])->vpisna = student->vpisna;
+                (opravili[k])->ocena = (po[j]).ocena;
+                k++;
+            }
+        }
+    }
+
+    stVO[0] = k;
+
+    //sortiranje
+    
+
+    return opravili;
 }
 
 #ifndef test
