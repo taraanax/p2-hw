e987747ce12800de0b5c763263440cadf06ec7a8
Tara Ana <tara.ana.fosnaric@gmail.com>
2026-08-10 03:03:07 +0200

    need to fix output syntax

--- DN/DN11/naloga2/naloga2.c
+++ DN/DN11/naloga2/naloga2.c
@@ -6,9 +6,49 @@
 
 #include "naloga2.h"
 
+int izpisiC(C* c, char* cilj);
+int izpisiB(B* b, char* cilj);
+int izpisiC(C* c, char* cilj);
+
 int izpisiA(A* a, char* cilj) {
-    // popravite / dopolnite ...
-    return -9999;
+    
+    if (a == NULL) printf("NULL");
+    else {
+        printf("%d ,", a->p);
+        izpisiB(a->b, cilj);
+    }
+    
+
+    return strlen(cilj);
+}
+
+int izpisiB(B* b, char* cilj) {
+    
+    if (b == NULL) printf("NULL");
+    else {
+        printf("%s ,", b->q);
+        izpisiC(b->c, cilj);
+    }
+    
+    return strlen(cilj);
+}
+
+int izpisiC(C* c, char* cilj) {
+    
+    if (c == NULL) printf("NULL");
+    else {
+        if (c->r) {
+            printf("true");
+            izpisiA(c->a, cilj);
+            izpisiB(c->b, cilj);
+        } else {
+            printf("false");
+            izpisiA(c->a, cilj);
+            izpisiB(c->b, cilj);
+        }
+    }
+
+    return strlen(cilj);
 }
 
 #ifndef test
