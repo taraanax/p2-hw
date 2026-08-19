40188d0aff544af227c578d0addabbda3b3f0714
Tara Ana <tara.ana.fosnaric@gmail.com>
2026-08-19 22:57:35 +0200

    10 / 10

--- izpiti/izpit_2026_2/naloga2/naloga2.c
+++ izpiti/izpit_2026_2/naloga2/naloga2.c
@@ -26,7 +26,42 @@
 // po potrebi dopolnite ...
 
 void razdeli(Vozlisce* vhod, Vozlisce** sodi, Vozlisce** lihi) {
-    // dopolnite ...
+
+    Vozlisce* sodRep = NULL;
+    Vozlisce* lihRep = NULL;
+
+    Vozlisce* curr = vhod;
+
+    while (curr != NULL) {
+
+        Vozlisce* nov = malloc(sizeof(Vozlisce));
+        nov->x = curr->x;
+        nov->nasl = NULL;
+        
+        if (curr->x % 2 == 0) {
+            
+            if(sodi[0] == NULL) {
+                sodi[0] = nov;
+                sodRep = sodi[0];
+            } else {
+                sodRep->nasl = nov;
+                sodRep = sodRep->nasl;
+            }
+        } else {
+
+            if(lihi[0] == NULL) {
+                lihi[0] = nov;
+                lihRep = lihi[0];
+            } else {
+                lihRep->nasl = nov;
+                lihRep = lihRep->nasl;
+            }
+        }
+
+        curr = curr->nasl;
+    }
+
+    return;
 }
 
 #ifndef test
