9e5fb54cd732ac16e2451f951259489cb002b1af
Tara Ana <tara.ana.fosnaric@gmail.com>
2026-08-19 22:57:56 +0200

    10 / 10

--- izpiti/izpit_2026_2/naloga3/naloga3.c
+++ izpiti/izpit_2026_2/naloga3/naloga3.c
@@ -20,7 +20,36 @@
 // po potrebi dopolnite ...
 
 int main(int argc, char** argv) {
-    // dopolnite ...
+    
+    FILE* vhodna =  fopen(argv[1], "rb");
+    FILE* izhodna =  fopen(argv[2], "wb");
+
+    char crka; int st;
+    fscanf(vhodna, "%c%d", &crka, &st);
+    fprintf(izhodna, "%c%d\n", crka, st);
+
+    int w, h;
+    fscanf(vhodna, "%d %d", &w, &h);
+    fprintf(izhodna, "%d %d\n", h, w);
+
+    int cifra;
+    fscanf(vhodna, "%d", &cifra);
+    fprintf(izhodna, "%d\n", cifra);
+
+    fgetc(vhodna);
+
+    int px = w * h;
+    unsigned char matrika[h][w];
+    fread(&matrika, 1, px, vhodna);
+
+    for (int i = w-1; i > -1; i--) {
+        for (int j = 0; j < h; j++) {
+            fputc(matrika[j][i], izhodna);
+        }
+    }
+
+    fclose(vhodna);
+    fclose(izhodna);
 
     return 0;
 }
