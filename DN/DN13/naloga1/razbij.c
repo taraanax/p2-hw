b82738e5c1adad92a68d71afa951a533b0ef3607
Tara Ana <tara.ana.fosnaric@gmail.com>
2026-08-19 02:05:46 +0200

    2 / 10

--- DN/DN13/naloga1/razbij.c
+++ DN/DN13/naloga1/razbij.c
@@ -4,6 +4,26 @@
 
 int main (int argc, char* argv[]) {
 
+    FILE* datoteka = fopen(argv[1], "rb");
+    int k = atoi(argv[2]);
 
+    int i = 0;
+    char temp[k];
+    int bajti;
+    
+    while (bajti = fread(temp, 1, k, datoteka) > 0) {
+        char ime[100];
+        sprintf(ime, "%s.%d", argv[1], i);
+
+        FILE* dat = fopen(ime, "wb");
+        fwrite(temp, 1, bajti, dat);
+
+        fclose(dat);
+
+        i++;
+    }
+
+    fclose(datoteka);
+    
     return 0;
 }
