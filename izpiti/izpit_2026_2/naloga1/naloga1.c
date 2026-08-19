897df567c2e422e9d1f86e5259d24023d076c452
Tara Ana <tara.ana.fosnaric@gmail.com>
2026-08-19 19:32:37 +0200

    10 / 10

--- izpiti/izpit_2026_2/naloga1/naloga1.c
+++ izpiti/izpit_2026_2/naloga1/naloga1.c
@@ -17,10 +17,51 @@
 #include <stdbool.h>
 #include <string.h>
 
-// po potrebi dopolnite ...
+int sum(int len, int* arr) {
+    int sum = 0;
+
+    for (int i = 0; i < len; i++) {
+        sum += arr[i];
+    }
+
+    return sum;
+}
+
+void izpis (int* arr, int len) {
+    for (int i = 0; i < len; i++) {
+        printf("[%d]",arr[i]);
+    }
+
+    return;
+}
 
 int main(int argc, char** argv) {
-    // dopolnite ...
+    
+    int* neg = calloc(500, sizeof(int));
+    int* poz = calloc(500, sizeof(int));
+
+    poz[0] = atoi(argv[1]);
+    int j = 1;
+    int k = 0;
+
+    for (int i = 2; i < argc - 1; i++) {
+        if (strcmp(argv[i],"-") == 0) {
+            neg[k] = atoi(argv[i + 1]);
+            k++;
+        } else if (strcmp(argv[i],"+") == 0){
+            poz[j] = atoi(argv[i + 1]);
+            j++;
+        }
+    }
+
+    //izpis(poz, j);
+    //izpis(neg, k);
+
+    int sum1= sum(k, neg);
+    int sum2 = sum(j, poz);
+    int sum = sum2 - sum1;
+    
+    printf("%d", sum);
 
     return 0;
 }
