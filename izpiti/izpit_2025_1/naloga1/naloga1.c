0dd957b9e1943b2ff91a0f25fe1aa85e3d9e6ffd
Tara Ana <tara.ana.fosnaric@gmail.com>
2026-08-19 03:17:03 +0200

    10 / 10

--- izpiti/izpit_2025_1/naloga1/naloga1.c
+++ izpiti/izpit_2025_1/naloga1/naloga1.c
@@ -20,7 +20,33 @@
 // po potrebi dopolnite ...
 
 int main(int argc, char** argv) {
-    // dopolnite ...
+    
+    FILE* vhod = fopen(argv[1], "r");
+    FILE* izhod = fopen(argv[2], "w");
+
+    char stevilo[1000001];
+    int* stevila = malloc(100000 * sizeof(int));
+
+    int len = 0;
+    while(fgets(stevilo, 1000001, vhod) != NULL) {
+        stevila[len] = atoi(stevilo);
+        len++;
+    }
+
+
+    int max = -100000000;
+    int curr = 0;
+    for (int i = 0; i < len - 2; i++) {
+        curr = stevila[i] + stevila[i + 1] + stevila[i + 2];
+        if(curr > max) max = curr;
+    }
+
+    char resitev[1000001];
+    sprintf(resitev, "%d", max);
+    fputs(resitev, izhod);
+
+    fclose(vhod);
+    fclose(izhod);
 
     return 0;
 }
